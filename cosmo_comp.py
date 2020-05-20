7e8314a63dcd0c349da8926bd11ba449a26b7e80
Sulla2012 <jackorlowskischerer@gmail.com>
2020-05-20 14:18:20 -0400

    Fixed bug where the interval size of the log integrator was wrong

--- cosmo_comp.py
+++ cosmo_comp.py
@@ -10,7 +10,8 @@ def log_int(function, x0, xf, nstep = 1000):
     integral = 0
     domain = np.logspace(x0, xf, nstep)
     for i in range(len(domain)-1): 
-        integral += function(domain[i])*(np.log(domain[i+1]-domain[i]))
+        mtemp = (domain[i+1]-domain[i])/2
+        integral += function(domain[i])*(np.log(domain[i+1])-np.log(domain[i]))
     return integral
 
 def zint(mass, zmin, zmax, nstep = 1000):
@@ -23,16 +24,18 @@ def zint(mass, zmin, zmax, nstep = 1000):
         mass = np.log(mass)
 
     for i in range(len(z)-1):
-        integrand = lambda m: mass_function.massFunction(m, z[i], mdef = '200m', model = 'tinker08', q_out = 'dndlnM')
+        ztemp = (z[i+1]+z[i])/2
+        print(i)
+        integrand = lambda m: mass_function.massFunction(m, ztemp, mdef = '200m', model = 'tinker08', q_out = 'dndlnM')
         integrated_density = log_int(integrand, mass, 20.5)
         #Comoving volume computed over full sky i.e. 4pi steradians. Scale accordingly
-        comov_vol = Planck15.comoving_volume(z[i]).value
+        comov_vol = Planck15.comoving_volume(ztemp).value
         integral += integrated_density*comov_vol*(z[i+1]-z[i])
     return integral
 
 cosmology.setCosmology('planck15')
 
-print(zint(15, 0.1, 0.2, nstep = 10000))
+print(zint(14, 0.1, 0.2, nstep = 1000))
 
 
 
